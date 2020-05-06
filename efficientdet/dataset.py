75bdfb7779a1b9d9883b71978e5e06b8542476af
levanpon98 <levanpon98@gmail.com>
2020-05-06 17:07:41 +0700

    init source

---
+++ efficientdet/dataset.py
@@ -0,0 +1,181 @@
+import os
+import torch
+import pandas as pd
+import numpy as np
+
+from torch.utils.data import Dataset, DataLoader
+import cv2
+
+
+class WheatDataset(Dataset):
+
+    def __init__(self, dataframe, image_dir, transforms=None):
+        super().__init__()
+
+        self.image_ids = dataframe['image_id'].unique()
+        self.df = dataframe
+        self.image_dir = image_dir
+        self.transforms = transforms
+
+    def __getitem__(self, index: int):
+        image_id = self.image_ids[index]
+        records = self.df[self.df['image_id'] == image_id]
+
+        image = cv2.imread(f'{self.image_dir}/{image_id}.jpg', cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
+
+        boxes = records[['x', 'y', 'w', 'h']].values
+        boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
+        boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
+
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        area = torch.as_tensor(area, dtype=torch.float32)
+
+        # there is only one class
+        labels = torch.ones((records.shape[0],), dtype=torch.int64)
+
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((records.shape[0],), dtype=torch.int64)
+
+        target = {}
+        target['boxes'] = boxes
+        target['labels'] = labels
+        # target['masks'] = None
+        target['image_id'] = torch.tensor([index])
+        target['area'] = area
+        target['iscrowd'] = iscrowd
+        sample = {
+            'image': image,
+            'bboxes': target['boxes']
+        }
+        if self.transforms:
+            sample = self.transforms(sample)
+
+        return sample
+
+    def __len__(self) -> int:
+        return self.image_ids.shape[0]
+
+
+class WheatTestDataset(Dataset):
+
+    def __init__(self, dataframe, image_dir, transforms=None):
+        super().__init__()
+
+        self.image_ids = dataframe['image_id'].unique()
+        self.df = dataframe
+        self.image_dir = image_dir
+        self.transforms = transforms
+
+    def __getitem__(self, index: int):
+        image_id = self.image_ids[index]
+        records = self.df[self.df['image_id'] == image_id]
+
+        image = cv2.imread(f'{self.image_dir}/{image_id}.jpg', cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
+
+        if self.transforms:
+            sample = {
+                'image': image,
+            }
+            sample = self.transforms(**sample)
+            image = sample['image']
+
+        return image, image_id
+
+    def __len__(self) -> int:
+        return self.image_ids.shape[0]
+
+
+def collater(data):
+    imgs = [s['image'] for s in data]
+    annots = [s['bboxes'] for s in data]
+    scales = [s['scale'] for s in data]
+
+    imgs = torch.from_numpy(np.stack(imgs, axis=0))
+
+    max_num_annots = max(annot.shape[0] for annot in annots)
+
+    if max_num_annots > 0:
+
+        annot_padded = torch.ones((len(annots), max_num_annots, 4)) * -1
+
+        for idx, annot in enumerate(annots):
+            if annot.shape[0] > 0:
+                annot_padded[idx, :annot.shape[0], :] = annot
+    else:
+        annot_padded = torch.ones((len(annots), 1, 5)) * -1
+
+    imgs = imgs.permute(0, 3, 1, 2)
+
+    return {'image': imgs, 'bboxes': annot_padded, 'scale': scales}
+
+
+class Resizer(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __init__(self, img_size=512):
+        self.img_size = img_size
+
+    def __call__(self, sample):
+        image, annots = sample['image'], sample['bboxes']
+        height, width, _ = image.shape
+        if height > width:
+            scale = self.img_size / height
+            resized_height = self.img_size
+            resized_width = int(width * scale)
+        else:
+            scale = self.img_size / width
+            resized_height = int(height * scale)
+            resized_width = self.img_size
+
+        image = cv2.resize(image, (resized_width, resized_height), interpolation=cv2.INTER_LINEAR)
+
+        new_image = np.zeros((self.img_size, self.img_size, 3))
+        new_image[0:resized_height, 0:resized_width] = image
+
+        annots[:, :4] *= scale
+
+        return {'image': torch.from_numpy(new_image).to(torch.float32), 'bboxes': torch.from_numpy(annots),
+                'scale': scale}
+
+
+class Augmenter(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample, flip_x=0.5):
+        if np.random.rand() < flip_x:
+            image, annots = sample['image'], sample['bboxes']
+            image = image[:, ::-1, :]
+
+            rows, cols, channels = image.shape
+
+            x1 = annots[:, 0].copy()
+            x2 = annots[:, 2].copy()
+
+            x_tmp = x1.copy()
+
+            annots[:, 0] = cols - x2
+            annots[:, 2] = cols - x_tmp
+
+            sample = {'image': image, 'bboxes': annots}
+
+        return sample
+
+
+class Normalizer(object):
+
+    def __init__(self, mean=None, std=None):
+        if std is None:
+            std = [0.229, 0.224, 0.225]
+        if mean is None:
+            mean = [0.485, 0.456, 0.406]
+        self.mean = np.array([[mean]])
+        self.std = np.array([[std]])
+
+    def __call__(self, sample):
+        image, annots = sample['image'], sample['bboxes']
+
+        return {'image': ((image.astype(np.float32) - self.mean) / self.std), 'bboxes': annots}
---
+++ utils/preprocess.py
@@ -0,0 +1,32 @@
+import re
+import numpy as np
+import pandas as pd
+
+
+def get_train_val(train_df):
+    train_df['x'] = -1
+    train_df['y'] = -1
+    train_df['w'] = -1
+    train_df['h'] = -1
+
+    def expand_bbox(x):
+        r = np.array(re.findall("([0-9]+[.]?[0-9]*)", x))
+        if len(r) == 0:
+            r = [-1, -1, -1, -1]
+        return r
+
+    train_df[['x', 'y', 'w', 'h']] = np.stack(train_df['bbox'].apply(lambda x: expand_bbox(x)))
+    train_df.drop(columns=['bbox'], inplace=True)
+    train_df['x'] = train_df['x'].astype(np.float)
+    train_df['y'] = train_df['y'].astype(np.float)
+    train_df['w'] = train_df['w'].astype(np.float)
+    train_df['h'] = train_df['h'].astype(np.float)
+
+    image_ids = train_df['image_id'].unique()
+    valid_ids = image_ids[-665:]
+    train_ids = image_ids[:-665]
+
+    valid_df = train_df[train_df['image_id'].isin(valid_ids)]
+    train_df = train_df[train_df['image_id'].isin(train_ids)]
+
+    return train_df, valid_df
