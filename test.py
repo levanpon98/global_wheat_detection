b869845cee7bbcfa52877f46f9bbdd91a6637e83
levanpon98 <levanpon98@gmail.com>
2020-05-07 09:31:57 +0700

    update

---
+++ test.py
@@ -0,0 +1,68 @@
+import torch
+from torch.backends import cudnn
+from backbone import EfficientDetBackbone
+import pandas as pd
+import os
+from efficientdet.utils import BBoxTransform, ClipBoxes
+from utils.utils import preprocess, invert_affine, postprocess, STANDARD_COLORS, standard_to_bgr, get_index_label, \
+    plot_one_box
+
+compound_coef = 1
+force_input_size = None
+
+anchor_ratios = [(1.0, 1.0), (1.4, 0.7), (0.7, 1.4)]
+anchor_scales = [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)]
+
+threshold = 0.2
+iou_threshold = 0.2
+
+use_cuda = True
+use_float16 = False
+cudnn.fastest = True
+cudnn.benchmark = True
+
+obj_list = ['wheat']
+
+color_list = standard_to_bgr(STANDARD_COLORS)
+input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536]
+input_size = input_sizes[compound_coef] if force_input_size is None else force_input_size
+
+model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
+                             ratios=anchor_ratios, scales=anchor_scales)
+model.load_state_dict(torch.load(f'weights/efficientdet-d{compound_coef}.pth'))
+model.requires_grad_(False)
+model.eval()
+
+if use_cuda:
+    model = model.cuda()
+if use_float16:
+    model = model.half()
+
+submit_df = pd.read_csv('/home/levanpon/data/global-wheat-detection/sample_submission.csv')
+
+for root, _, files in os.walk('/home/levanpon/data/global-wheat-detection/test'):
+    for file in files:
+        img_path = os.path.join(root, file)
+
+        ori_imgs, framed_imgs, framed_metas = preprocess(img_path, max_size=input_size)
+
+        if use_cuda:
+            x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
+        else:
+            x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
+
+        x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
+
+        with torch.no_grad():
+            features, regression, classification, anchors = model(x)
+
+            regressBoxes = BBoxTransform()
+            clipBoxes = ClipBoxes()
+
+            out = postprocess(x,
+                              anchors, regression, classification,
+                              regressBoxes, clipBoxes,
+                              threshold, iou_threshold)
+
+            out = invert_affine(framed_metas, out)
+            print(out)
