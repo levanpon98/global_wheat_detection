38763d40d24fc181258553a6cd73d9096b588158
levanpon98 <levanpon98@gmail.com>
2020-05-07 09:54:16 +0700

    update

--- test.py
+++ test.py
@@ -40,7 +40,7 @@ if use_cuda:
 if use_float16:
     model = model.half()
 
-submit_df = pd.read_csv('../input/global-wheat-detection/sample_submission.csv')
+submit_df = pd.read_csv('../../input/global-wheat-detection/sample_submission.csv')
 
 
 def display(preds, imgs, imwrite=True):
@@ -61,7 +61,7 @@ def display(preds, imgs, imwrite=True):
 
 detection_threshold = 0.5
 results = []
-for root, _, files in os.walk('../input/global-wheat-detection/test'):
+for root, _, files in os.walk('../../input/global-wheat-detection/test'):
     for file in files:
         img_path = os.path.join(root, file)
 
